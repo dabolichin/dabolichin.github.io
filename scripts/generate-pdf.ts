9aa4f999e7650f89b16b2323b8860d16b6f42a11
Dmitrij Abolichin <dmitrij.abolichin@kilo.health>
2023-02-24 13:22:10 +0200

    Fix pdf generation

--- scripts/generate-pdf.ts
+++ scripts/generate-pdf.ts
@@ -4,10 +4,10 @@ import { chromium } from 'playwright';
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
-  await page.goto('http://127.0.0.1:3000/', { waitUntil: 'networkidle' });
+  await page.goto('http://localhost:3000/', { waitUntil: 'networkidle' });
 
-  //const downloadButton = page.locator('download');
-  //await downloadButton.evaluate((node) => (node.innerHTML = ''));
+  const downloadButton = page.locator('a[download]');
+  await downloadButton.evaluate((node) => (node.innerHTML = ''));
 
   const body = page.locator('body');
   await body.evaluate((node) => node.classList.remove('bg-indigo-50'));
