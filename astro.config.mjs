40a377f35ea1f9903a60994596e68ec9a56a1d70
Dmitrij Abolichin <d.abolichin@gmail.com>
2024-02-03 01:09:12 +0200

    Bump astro-icon version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    icon()
+  ],
   vite: {
     ssr: {
       external: ['svgo'],
